080c6bdce55a15039618c1579cc46c670e6f88a3
Rajesh Dhiman <rajeshdhimanskt@gmail.com>
2020-12-13 20:22:18 +0530

    implemented the functionality

--- bizarreStringIncrementer.js
+++ bizarreStringIncrementer.js
@@ -1,1 +1,31 @@
 // Start your implementation here
+const bizarreStringIncrementer = (str) => {
+    if(!str || typeof(str) !== 'string') return 'Please provide a valid string';
+
+    // find digits in the end of the string
+    let numberMatches = str.match(/\d+$/);
+    if (!numberMatches) {
+        return str + '1';
+    }
+    let endNumStr = numberMatches[0];
+    let endNum = parseInt(endNumStr, 10);
+
+    if (endNumStr != endNum.toString()) {
+        // need padding 
+        let zerosCount = endNumStr.length ;
+        endNum++;
+        let newStr = endNum.toString();
+        newStr = newStr.padStart(zerosCount, "0");
+        str = str.replace(endNumStr, newStr);
+
+        return str;
+    } else {
+        endNum++;
+        str = str.replace(endNumStr, endNum);
+        return str;
+
+    }
+
+}
+
+module.exports = bizarreStringIncrementer;
--- bizarreStringIncrementer.test.js
+++ bizarreStringIncrementer.test.js
@@ -1,4 +1,4 @@
-import { bizarreStringIncrementer } from './bizarreStringIncrementer';
+const bizarreStringIncrementer = require('./bizarreStringIncrementer.js')
 
 describe('bizarreStringIncrementer', () => {
   it('bizarreStringIncrementer adds the number 1 if string does not end in a number', () => {
